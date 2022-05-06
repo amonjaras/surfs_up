0dd8ab9c8c94bdb2b5640a1383e99d1828423f1b
amonjaras <100452376+amonjaras@users.noreply.github.com>
2022-05-06 16:44:52 -0400

    adding app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from cProfile import run
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_wold():
+    return 'Hello world'
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
